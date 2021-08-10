fb666ef908763313b4afecf98fe65fdbdf22347a
Matt Elstone <matthew_elstone@hotmail.com>
2021-08-10 17:09:47 +0000

    added stylesheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+//-----adding constants for the home page
+const startButton = document.getElementById("start-test");
+const home = document.getElementById("home");
+const questionBox = document.getElementById("test-container");
+const question = document.getElementById("question-container")
+const answerButtons = document.getElementById("answer-buttons");
+const submitButton = document.getElementById("")
+
+startButton.addEventListener("click", startTest);
+
+function startTest(){
+
+};
