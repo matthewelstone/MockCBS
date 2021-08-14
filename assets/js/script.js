b72e553718ac3200a9d87a21eb546219e09eb27c
Matt <matthew_elstone@hotmail.com>
2021-08-14 14:05:18 +0100

    got rid of most of the javascript as am changing the way that the quiz works

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,50 +4,47 @@ const home = document.getElementById("home");
 const questionBox = document.getElementById("test-container");
 const questionElement = document.getElementById("question-container")
 const answerButtons = document.getElementById("answer-buttons");
-const submitButton = document.getElementById("submit-div");
-
-let randomisedQuestions, currentQuestions
-
-startButton.addEventListener("click", startTest);
-
-function startTest(){
-    startButton.classList.add("hide");
-    home.classList.add("hide");
-    questionBox.classList.remove("hide");
-    submitButton.classList.remove("hide");
-    randomisedQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestions = 0;
-    nextQuestion()
-
-};
-
-function nextQuestion(){
-    showQuestion(randomisedQuestions[currentQuestions])
-
-};
-
-function showQuestion(question){
-    questionElement.innerText = question.question
-   
-    
-
-};
-
-
-function selectAnswer(){
-
-}
+const nextButton = document.getElementById("submit-div");
+const score = document.getElementById("score");
 
 
 // -------- Questions
 const questions = [
     {
         question: "In the three tier system for alcohol sales, an Importer can sell to?",
-        answers: [
-            {text:"retailers", correct:"false"},
-            {text:"Wholesalers", correct:"true"},
-            {text:"Breweries", correct:"false"},
-            {text:"Consumers", correct:"false"}
-        ]
-    }
+        options: ["Retailers",
+                "Wholesalers",
+                "Breweries",
+                "Consumers"],
+        answer: "Wholesalers"},
+       
+    {  
+        question: "Which of these beers has the highest perceived bitterness?",
+        options: ["Kolsch",
+                "Bohemian Pilsner",
+                "Doppelbock",
+                "German Pilsner"],
+        answer: "German Pilsner"},
+    {
+        question:"a common exception to the three tier system for alcohol sales is?",
+        options: ["A bar that runs out of a certain beer can buy it at a grocery storeand sell it to consumers", 
+                "A bottle shop can go to a special bottle release at a brewery, purchase bottles and sell them to consumers",
+                "A brewery can operate a taproom where they can sell to consumers",
+                "Saying 'It's cool just keep it on the down low'."],
+        answer:"A brewery can operate a taproom where they can sell to consumers"},
+    {
+        question:"If beer is pouring foamy at the tap, which of these could not be the cause?",
+        options: ["A kink in the beer line",
+                "The beer is too cold",
+                "The keg is not under enough pressure",
+                "The beer lines are too clean"],
+        answer:"The beer lines are too clean"},
+    {
+        question:"Which is not an aspect of mouthfeel?",
+        options:["Thinness",
+                "Body",
+                "Bitterness",
+                "Carbonation"],
+        answer:"Bitterness"},
+    {}
 ]
