71e63967e90ec56787a8c3d7cd29aa437d4a955b
Matt <matthew_elstone@hotmail.com>
2021-08-14 14:29:28 +0100

    questions are now added to available questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
-//-----adding constants for the home page
-const startButton = document.getElementById("start-test");
-const home = document.getElementById("home");
-const questionBox = document.getElementById("test-container");
-const questionElement = document.getElementById("question-container")
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("submit-div");
-const score = document.getElementById("score");
-
-
 // -------- Questions
 const questions = [
     {
@@ -16,35 +6,66 @@ const questions = [
                 "Wholesalers",
                 "Breweries",
                 "Consumers"],
-        answer: "Wholesalers"},
-       
+        answer:1
+    },   
     {  
         question: "Which of these beers has the highest perceived bitterness?",
         options: ["Kolsch",
                 "Bohemian Pilsner",
                 "Doppelbock",
                 "German Pilsner"],
-        answer: "German Pilsner"},
+        answer: 3
+    },
     {
         question:"a common exception to the three tier system for alcohol sales is?",
         options: ["A bar that runs out of a certain beer can buy it at a grocery storeand sell it to consumers", 
                 "A bottle shop can go to a special bottle release at a brewery, purchase bottles and sell them to consumers",
                 "A brewery can operate a taproom where they can sell to consumers",
                 "Saying 'It's cool just keep it on the down low'."],
-        answer:"A brewery can operate a taproom where they can sell to consumers"},
+        answer:2
+    },
     {
         question:"If beer is pouring foamy at the tap, which of these could not be the cause?",
         options: ["A kink in the beer line",
                 "The beer is too cold",
                 "The keg is not under enough pressure",
                 "The beer lines are too clean"],
-        answer:"The beer lines are too clean"},
+        answer:3
+    },
     {
         question:"Which is not an aspect of mouthfeel?",
         options:["Thinness",
                 "Body",
                 "Bitterness",
                 "Carbonation"],
-        answer:"Bitterness"},
-    {}
+        answer:2
+    },
+    
 ]
+//-----adding constants 
+const startButton = document.getElementById("start-test");
+const home = document.getElementById("home");
+const questionBox = document.getElementById("test-container");
+const questionNumber = document.getElementById("question-number");
+const questionElement = document.getElementById("question-container");
+const options = document.getElementById("options");
+const nextButton = document.getElementById("submit-div");
+const score = document.getElementById("score");
+
+let questionCounter = 0;
+let currentQuestion ;
+let availableQuestions = [];
+
+function setAvailableQuestions(){
+        const totalQuestions = questions.length;
+        for(let i=0; i<totalQuestions; i++){
+            console.log(questions[i])
+        }
+};
+
+window.onload = function(){
+
+    setAvailableQuestions();
+}
+
+
